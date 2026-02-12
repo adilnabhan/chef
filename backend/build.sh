0056fa5090428e29c9b7d71d1bb360f1fcfe0bdf
adilnabhan <adilnabhanc.v@gmail.com>
2026-02-12 12:32:43 +0530

    Fix nested repo + admin auto create

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+set -o errexit
+
 pip install -r requirements.txt
+
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+echo "Creating admin user..."
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_ADMIN_USERNAME", "admin")
+password = os.environ.get("DJANGO_ADMIN_PASSWORD", "admin123")
+email = os.environ.get("DJANGO_ADMIN_EMAIL", "admin@example.com")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("SUPERUSER CREATED")
+else:
+    print("SUPERUSER EXISTS")
+EOF
