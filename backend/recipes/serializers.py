d309d969f916e2fa66c936168f252d3666442574
adilnabhan <adilnabhanc.v@gmail.com>
2026-02-12 11:12:53 +0530

    Fixed submodule issue - backend & frontend now normal folders

---
+++ backend/recipes/serializers.py
@@ -0,0 +1,25 @@
+﻿from rest_framework import serializers
+from .models import Recipe, RecipeCategory, ChatHistory
+
+class RecipeCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeCategory
+        fields = '__all__'
+
+class RecipeSerializer(serializers.ModelSerializer):
+    category = RecipeCategorySerializer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=RecipeCategory.objects.all(),
+        source='category',
+        write_only=True
+    )
+    
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+class ChatHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatHistory
+        fields = '__all__'
+        read_only_fields = ['user']
